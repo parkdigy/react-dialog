94e0ebc118a7f93ac13afd7c5d21e2653064b3b5
parkdigy <parkdigy@gmail.com>
2025-06-07 15:39:26 +0900

    1.0.31

--- dist/AlertDialog/AlertDialog.types.d.ts
+++ dist/AlertDialog/AlertDialog.types.d.ts
@@ -3,7 +3,7 @@ import { ButtonProps } from '@mui/material';
 import { DialogCommands, DialogProps } from '../Dialog';
 export interface AlertDialogCommands extends Omit<DialogCommands, 'scrollToTop'> {
 }
-export interface AlertDialogProps extends Omit<DialogProps, 'commandsRef' | 'actions' | 'autoClose' | 'backdropClose' | 'escapeClose' | 'onRequestClose'> {
+export interface AlertDialogProps extends Omit<DialogProps, 'commandsRef' | 'actions' | 'autoClose' | 'escapeClose' | 'onRequestClose'> {
     confirmButtonLabel?: ReactNode;
     confirmButtonProps?: Omit<ButtonProps, 'children' | 'onClick'>;
     onCommands?(commands: AlertDialogCommands): void;
--- src/AlertDialog/AlertDialog.types.ts
+++ src/AlertDialog/AlertDialog.types.ts
@@ -5,10 +5,7 @@ import { DialogCommands, DialogProps } from '../Dialog';
 export interface AlertDialogCommands extends Omit<DialogCommands, 'scrollToTop'> {}
 
 export interface AlertDialogProps
-  extends Omit<
-    DialogProps,
-    'commandsRef' | 'actions' | 'autoClose' | 'backdropClose' | 'escapeClose' | 'onRequestClose'
-  > {
+  extends Omit<DialogProps, 'commandsRef' | 'actions' | 'autoClose' | 'escapeClose' | 'onRequestClose'> {
   confirmButtonLabel?: ReactNode;
   confirmButtonProps?: Omit<ButtonProps, 'children' | 'onClick'>;
   onCommands?(commands: AlertDialogCommands): void;
